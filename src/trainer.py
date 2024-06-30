ae8c98d9ae6171fdd1f29882e9672fc2d80a1af2
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-06-30 23:06:31 +0200

    CLI is added in the trainer

--- src/trainer.py
+++ src/trainer.py
@@ -31,7 +31,7 @@ class Trainer:
         weight_decay=0.0001,
         momentum=0.9,
         adversarial_lambda=0.001,
-        pixelwise_lamda=0.999,
+        pixelwise_lambda=0.999,
         steps=10,
         step_size=10,
         gamma=0.1,
@@ -63,7 +63,7 @@ class Trainer:
         self.display = display
         self.is_weight_init = is_weight_init
         self.adversarial_lambda = adversarial_lambda
-        self.pixelwise_lamda = pixelwise_lamda
+        self.pixelwise_lambda = pixelwise_lambda
         self.steps = steps
         self.step_size = step_size
         self.gamma = gamma
@@ -179,7 +179,7 @@ class Trainer:
 
         total_netG_loss = (
             self.adversarial_lambda * predicted_inpaint_loss
-            + self.pixelwise_lamda * pixelwise_loss
+            + self.pixelwise_lambda * pixelwise_loss
         )
 
         if self.l1_regularization:
@@ -386,27 +386,150 @@ class Trainer:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Trainer for Context Encoder".title())
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=config()["trainer"]["epochs"],
+        help="Define the epochs for training".capitalize(),
+    )
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=config()["trainer"]["lr"],
+        help="Learning rate".capitalize(),
+    )
+    parser.add_argument(
+        "--beta1",
+        type=float,
+        default=config()["trainer"]["beta1"],
+        help="Beta1 for Adam optimizer".capitalize(),
+    )
+    parser.add_argument(
+        "--beta2",
+        type=float,
+        default=config()["trainer"]["beta2"],
+        help="Beta2 for Adam optimizer".capitalize(),
+    )
+    parser.add_argument(
+        "--weight_decay",
+        type=float,
+        default=config()["trainer"]["weight_decay"],
+        help="Weight decay".capitalize(),
+    )
+    parser.add_argument(
+        "--momentum",
+        type=float,
+        default=config()["trainer"]["momentum"],
+        help="Momentum for SGD".capitalize(),
+    )
+    parser.add_argument(
+        "--adversarial_lambda",
+        type=float,
+        default=config()["trainer"]["adversarial_lambda"],
+        help="Adversarial lambda".capitalize(),
+    )
+    parser.add_argument(
+        "--pixelwise_lambda",
+        type=float,
+        default=config()["trainer"]["pixelwise_lambda"],
+        help="Pixelwise lambda".capitalize(),
+    )
+    parser.add_argument(
+        "--steps",
+        type=int,
+        default=config()["trainer"]["steps"],
+        help="Number of steps".capitalize(),
+    )
+    parser.add_argument(
+        "--step_size",
+        type=int,
+        default=config()["trainer"]["step_size"],
+        help="Step size for learning rate scheduler".capitalize(),
+    )
+    parser.add_argument(
+        "--gamma",
+        type=float,
+        default=config()["trainer"]["gamma"],
+        help="Gamma for learning rate scheduler".capitalize(),
+    )
+    parser.add_argument(
+        "--device",
+        type=str,
+        default=config()["trainer"]["device"],
+        help="Device to run training on".capitalize(),
+    )
+    parser.add_argument(
+        "--adam",
+        type=bool,
+        default=config()["trainer"]["adam"],
+        help="Use Adam optimizer".capitalize(),
+    )
+    parser.add_argument(
+        "--SGD",
+        type=bool,
+        default=config()["trainer"]["SGD"],
+        help="Use SGD optimizer".capitalize(),
+    )
+    parser.add_argument(
+        "--l1_regularization",
+        type=bool,
+        default=config()["trainer"]["l1_regularization"],
+        help="Use L1 regularization".capitalize(),
+    )
+    parser.add_argument(
+        "--l2_regularization",
+        type=bool,
+        default=config()["trainer"]["l2_regularization"],
+        help="Use L2 regularization".capitalize(),
+    )
+    parser.add_argument(
+        "--lr_scheduler",
+        type=bool,
+        default=config()["trainer"]["lr_scheduler"],
+        help="Use learning rate scheduler".capitalize(),
+    )
+    parser.add_argument(
+        "--MLFlow",
+        type=bool,
+        default=config()["trainer"]["MLFlow"],
+        help="Use MLFlow for logging".capitalize(),
+    )
+    parser.add_argument(
+        "--display",
+        type=bool,
+        default=config()["trainer"]["display"],
+        help="Display training progress".capitalize(),
+    )
+    parser.add_argument(
+        "--is_weight_init",
+        type=bool,
+        default=config()["trainer"]["is_weight_init"],
+        help="Initialize weights".capitalize(),
+    )
+
+    args = parser.parse_args()
+
     trainer = Trainer(
-        epochs=100,
-        lr=0.0002,
-        beta1=0.5,
-        beta2=0.999,
-        weight_decay=0.0001,
-        momentum=0.9,
-        adversarial_lambda=0.001,
-        pixelwise_lamda=0.999,
-        steps=10,
-        step_size=20,
-        gamma=0.5,
-        device="cuda",
-        adam=True,
-        SGD=False,
-        l1_regularization=False,
-        l2_regularization=False,
-        lr_scheduler=False,
-        MLFlow=True,
-        display=True,
-        is_weight_init=True,
+        epochs=args.epochs,
+        lr=args.lr,
+        beta1=args.beta1,
+        beta2=args.beta2,
+        weight_decay=args.weight_decay,
+        momentum=args.momentum,
+        adversarial_lambda=args.adversarial_lambda,
+        pixelwise_lambda=args.pixelwise_lambda,
+        steps=args.steps,
+        step_size=args.step_size,
+        gamma=args.gamma,
+        device=args.device,
+        adam=args.adam,
+        SGD=args.SGD,
+        l1_regularization=args.l1_regularization,
+        l2_regularization=args.l2_regularization,
+        lr_scheduler=args.lr_scheduler,
+        MLFlow=args.MLFlow,
+        display=args.display,
+        is_weight_init=args.is_weight_init,
     )
 
     trainer.train()
