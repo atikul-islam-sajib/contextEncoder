c9a15ae9adae04ad93ac063581255d30fba277de
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-06-30 14:13:15 +0200

    dataloader is created partially

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,9 +1,14 @@
 import os
 import sys
+import cv2
 import zipfile
+import traceback
 import torch
+from tqdm import tqdm
 from PIL import Image
 from torchvision import transforms
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
 
 sys.path.append("src/")
 
@@ -19,6 +24,9 @@ class Loader:
         self.image_size = image_size
         self.batch_size = batch_size
         self.split_size = split_size
+
+        self.X = []
+        self.y = []
 
         self.CONFIG = config()
 
@@ -65,6 +73,101 @@ class Loader:
                 ]
             )
 
+    def split_dataset(self, X, y):
+        if isinstance(X, list) and isinstance(y, list):
+            X_train, X_test, y_train, y_test = train_test_split(
+                X, y, test_size=self.split_size, random_state=42, shuffle=True
+            )
+
+            return X_train, X_test, y_train, y_test
+
+        else:
+            raise ValueError("X and y should be in the list format".capitalize())
+
+    def feature_extractor(self):
+        self.processed_data_path = self.CONFIG["path"]["PROCESSED_DATA_PATH"]
+
+        X = os.path.join(self.processed_data_path, "dataset", "X")
+        y = os.path.join(self.processed_data_path, "dataset", "y")
+
+        for image in tqdm(os.listdir(X)):
+            if image in os.listdir(y):
+                image_X = os.path.join(X, image)
+                image_Y = os.path.join(y, image)
+
+                if (image_X is not None) and (image_Y is not None):
+                    image_X = cv2.imread(image_X)
+                    image_Y = cv2.imread(image_Y)
+
+                    image_X = cv2.cvtColor(image_X, cv2.COLOR_BGR2RGB)
+                    image_Y = cv2.cvtColor(image_Y, cv2.COLOR_BGR2RGB)
+
+                    image_X = Image.fromarray(image_X)
+                    image_Y = Image.fromarray(image_Y)
+
+                    image_X = self.transforms(type="train")(image_X)
+                    image_Y = self.transforms(type="valid")(image_Y)
+
+                    self.X.append(image_X)
+                    self.y.append(image_Y)
+
+                else:
+                    raise Exception(
+                        "Image {} is not found in the path of {}".format(
+                            image, self.processed_data_path
+                        )
+                    )
+
+        try:
+            X_train, X_test, y_train, y_test = self.split_dataset(X=self.X, y=self.y)
+
+        except ValueError as e:
+            print(e)
+            traceback.print_exc()
+
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+
+        else:
+            return {
+                "X_train": X_train,
+                "X_test": X_test,
+                "y_train": y_train,
+                "y_test": y_test,
+            }
+
+    def create_dataloader(self):
+        dataset = self.feature_extractor()
+
+        train_dataloader = DataLoader(
+            dataset=list(zip(dataset["X_train"], dataset["y_train"])),
+            batch_size=self.batch_size,
+            shuffle=True,
+        )
+        test_dataloader = DataLoader(
+            dataset=list(zip(dataset["X_test"], dataset["y_test"])),
+            batch_size=self.batch_size,
+            shuffle=True,
+        )
+
+        for value, filename in [
+            (train_dataloader, "train_dataloader"),
+            (test_dataloader, "test_dataloader"),
+        ]:
+            dump(
+                value=value,
+                filename=os.path.join(
+                    config()["path"]["PROCESSED_DATA_PATH"], filename + ".pkl"
+                ),
+            )
+
+        print(
+            "data is stored in the folder {}".format(
+                config()["path"]["PROCESSED_DATA_PATH"]
+            )
+        )
+
 
 if __name__ == "__main__":
     loader = Loader(
@@ -74,4 +177,6 @@ if __name__ == "__main__":
         split_size=0.25,
     )
 
-    loader.unzip_folder()
+    # loader.unzip_folder()
+    # loader.feature_extractor()
+    loader.create_dataloader()
